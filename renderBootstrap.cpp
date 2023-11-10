5e2c54226c5aaab4bc72f22bbbbee5ffd4ca945b
odiewan <odie@odiesystems.com>
2023-11-09 16:44:03 -0800

    implement button groups

--- renderBootstrap.cpp
+++ renderBootstrap.cpp
@@ -3,13 +3,15 @@
 
 String buttonStyleNames[] = {
     "btn",
-    "btn-default",
-    "btn-primary",
-    "btn-success",
-    "btn-info",
-    "btn-warning",
-    "btn-danger",
-    "btn-link"
+    "btn btn-primary",
+    "btn btn-secondary",
+    "btn btn-success",
+    "btn btn-info",
+    "btn btn-warning",
+    "btn btn-danger",
+    "btn btn-dark",
+    "btn btn-light",
+    "btn btn-link"
 };
 
 //=================================================================================================
@@ -19,15 +21,142 @@ render_bootstrap::render_bootstrap()
 }
 
 //=================================================================================================
-void render_bootstrap::renderButton(String nName, buttonStyles nStyle) {
-    String _html = "<button type='button' class='btn " + buttonStyleNames[nStyle] + "'>" + nName + "</button>";
+String render_bootstrap::genLinkButton(String nName, String nUrl, buttonStyles nStyle) {
+  return "<a href='" + nUrl + "' class='" + buttonStyleNames[nStyle] + "' role='button'>" + nName + "</a>";
+}
+
+  //=================================================================================================
+  void render_bootstrap::renderButton(String nName, buttonStyles nStyle) {
+    String _html = "<button type='button' class='" + buttonStyleNames[nStyle] + "'>" + nName + "</button>";
     rbsClient.print(_html);
 }
 
 //=================================================================================================
 void render_bootstrap::renderButtonLink(String nUrl, String nName, buttonStyles nStyle) {
-    String _html = "<a href='" + nUrl + "' class='btn " + buttonStyleNames[nStyle] + "'>" + nName + "</a>";
+    String _html = "<a href='" + nUrl + "' class='" + buttonStyleNames[nStyle] + "'>" + nName + "</a>";
     rbsClient.print(_html);
+}
+
+//=================================================================================================
+void render_bootstrap:: renderButtonGroup(String nUrl, String nName, buttonGroupStyles nBGStyle) {
+  String _html;
+  switch (nBGStyle) {
+    default:
+    case BGS_H_BINARY:
+    case BGS_H_TRINARY:
+    case BGS_H_FOUR_STATE:
+    case BGS_V_BINARY:
+    case BGS_V_TRINARY:
+    case BGS_V_FOUR_STATE:
+        break;
+
+    case BGS_H_ON_OFF:
+        _html = "<div class='btn-group'>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>ON</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>OFF</a>";
+        _html += "</div>";
+        break;
+
+    case BGS_H_COLOR_SET:
+        _html = "<div class='btn-group'>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MAX</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>+</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MID</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>-</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MIN</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>OFF</a>";
+
+        _html += "</div>";
+        break;
+
+    case BGS_H_COLOR_SET_RED:
+        _html = "<div class='btn-group'>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MAX</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>+</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MID</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>-</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MIN</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>OFF</a>";
+        _html += "</div>";
+        break;
+
+    case BGS_H_COLOR_SET_BLUE:
+         _html = "<div class='btn-group'>";
+         _html += "<a href='" + nUrl + "' class='btn btn-basic'>MAX</a>";
+         _html += "<a href='" + nUrl + "' class='btn btn-basic'>+</a>";
+         _html += "<a href='" + nUrl + "' class='btn btn-basic'>MID</a>";
+         _html += "<a href='" + nUrl + "' class='btn btn-basic'>-</a>";
+         _html += "<a href='" + nUrl + "' class='btn btn-basic'>MIN</a>";
+         _html += "<a href='" + nUrl + "' class='btn btn-basic'>OFF</a>";
+         _html += "</div>";
+         break;
+
+    case BGS_H_COLOR_SET_GREEN:
+        _html = "<div class='btn-group'>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MAX</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>+</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MID</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>-</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MIN</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>OFF</a>";
+        _html += "</div>";
+        break;
+
+    case BGS_V_ON_OFF:
+      _html = "<div class='btn-group-vertical'>";
+      _html += "<a href='" + nUrl + "' class='btn btn-basic'>ON</a>";
+      _html += "<a href='" + nUrl + "' class='btn btn-basic'>OFF</a>";
+      _html += "</div>";
+      break;
+
+    case BGS_V_COLOR_SET:
+        _html = "<div class='btn-group-vertical'>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MAX</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>+</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MID</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>-</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>MIN</a>";
+        _html += "<a href='" + nUrl + "' class='btn btn-basic'>OFF</a>";
+
+        _html += "</div>";
+        break;
+
+    case BGS_V_COLOR_SET_RED:
+        _html = "<div class='btn-group-vertical'>";
+        _html += genLinkButton("MAX", "#", BTN_STYLE_DANGER);
+        _html += genLinkButton("+", "#", BTN_STYLE_DANGER);
+        _html += genLinkButton("MID", "#", BTN_STYLE_DANGER);
+        _html += genLinkButton("-", "#", BTN_STYLE_DANGER);
+        _html += genLinkButton("MIN", "#", BTN_STYLE_DANGER);
+        _html += genLinkButton("OFF", "#", BTN_STYLE_DANGER);
+        _html += "</div>";
+        break;
+
+    case BGS_V_COLOR_SET_BLUE:
+      _html = "<div class='btn-group-vertical'>";
+      _html += genLinkButton("MAX", "#", BTN_STYLE_PRIMARY);
+      _html += genLinkButton("+", "#", BTN_STYLE_PRIMARY);
+      _html += genLinkButton("MID", "#", BTN_STYLE_PRIMARY);
+      _html += genLinkButton("-", "#", BTN_STYLE_PRIMARY);
+      _html += genLinkButton("MIN", "#", BTN_STYLE_PRIMARY);
+      _html += genLinkButton("OFF", "#", BTN_STYLE_PRIMARY);
+
+      _html += "</div>";
+      break;
+
+    case BGS_V_COLOR_SET_GREEN:
+      _html = "<div class='btn-group-vertical'>";
+      _html += genLinkButton("MAX", "#", BTN_STYLE_SUCCESS);
+      _html += genLinkButton("+", "#", BTN_STYLE_SUCCESS);
+      _html += genLinkButton("MID", "#", BTN_STYLE_SUCCESS);
+      _html += genLinkButton("-", "#", BTN_STYLE_SUCCESS);
+      _html += genLinkButton("MIN", "#", BTN_STYLE_SUCCESS);
+      _html += genLinkButton("OFF", "#", BTN_STYLE_SUCCESS);
+      _html += "</div>";
+      break;
+
+  }
+  rbsClient.print(_html);
 }
 
 //=================================================================================================
--- renderBootstrap.h
+++ renderBootstrap.h
@@ -8,38 +8,63 @@
 
 enum buttonStyles {
   BTN_STYLE_BASIC,
-  BTN_STYLE_DEFAULT,
   BTN_STYLE_PRIMARY,
+  BTN_STYLE_SECONDARY,
   BTN_STYLE_SUCCESS,
   BTN_STYLE_INFO,
   BTN_STYLE_WARNING,
   BTN_STYLE_DANGER,
+  BTN_STYLE_DARK,
+  BTN_STYLE_LIGHT,
   BTN_STYLE_LINK,
   NUM_BUTTON_STYLES
 };
 
-
+enum buttonGroupStyles {
+  BGS_H_BINARY,
+  BGS_H_TRINARY,
+  BGS_H_FOUR_STATE,
+  BGS_H_ON_OFF,
+  BGS_H_COLOR_SET,
+  BGS_H_COLOR_SET_RED,
+  BGS_H_COLOR_SET_BLUE,
+  BGS_H_COLOR_SET_GREEN,
+  BGS_V_BINARY,
+  BGS_V_TRINARY,
+  BGS_V_FOUR_STATE,
+  BGS_V_ON_OFF,
+  BGS_V_COLOR_SET,
+  BGS_V_COLOR_SET_RED,
+  BGS_V_COLOR_SET_BLUE,
+  BGS_V_COLOR_SET_GREEN,
+  NUM_BUTTON_GROUP_STYLES
+};
 
 class render_bootstrap {
-    public:
-        WiFiClient rbsClient;
+  public:
+    WiFiClient rbsClient;
 
-        render_bootstrap();
+    render_bootstrap();
 
-        void renderCont(String s);
-        void renderButton(String nName, buttonStyles nStyle);
-        void renderButtonLink(String nUrl, String nName, buttonStyles nStyle);
-        void renderAryCont(String ary[]);
-        void renderH(int hLvl, String s);
-        void renderHDiv(int hLvl, String s);
-        void renderLink(String nUrl, String nName);
-        void renderDataRowCell(String st);
-        void renderH1(String s);
-        void renderH2(String s);
-        void renderH3(String s);
-        void renderH4(String s);
-        void renderH5(String s);
-        void setClient(WiFiClient nClient);
+    void renderCont(String s);
+    void renderButton(String nName, buttonStyles nStyle);
+    void renderButtonLink(String nUrl, String nName, buttonStyles nStyle);
+    void renderButtonGroup(String nUrl, String nName, buttonGroupStyles nBGStyle);
+
+    void renderAryCont(String ary[]);
+    void renderH(int hLvl, String s);
+    void renderHDiv(int hLvl, String s);
+    void renderLink(String nUrl, String nName);
+    void renderDataRowCell(String st);
+    void renderH1(String s);
+    void renderH2(String s);
+    void renderH3(String s);
+    void renderH4(String s);
+    void renderH5(String s);
+    void setClient(WiFiClient nClient);
+
+  private:
+   String genLinkButton(String nName, String nUrl, buttonStyles nStyle);
 };
 
 #endif
